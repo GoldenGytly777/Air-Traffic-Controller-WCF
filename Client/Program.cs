90ab39c8536aab063be2b8cb8044058aa6683bc8
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-25 21:33:59 +0200

    Added client implementation

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Client.ServiceReference1;
 
-namespace Client
+namespace GettingStartedClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Step 1: Create an instance of the WCF proxy.
+            Service1Client client = new Service1Client();
+
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            do
+            {
+                Console.WriteLine("Enter airport: ");
+                string cityFrom = Console.ReadLine();
+
+                Console.WriteLine("Enter airport destination: ");
+                string cityTo = Console.ReadLine();
+
+                Console.WriteLine("Scope from: ");
+                int scopeFrom = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Scope to: ");
+                int scopeTo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(client.getFlightInScope(cityFrom, cityTo, scopeFrom, scopeTo));
+
+                Console.WriteLine("Type ESC to exit");
+             
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            client.Close();
         }
+        
     }
 }
